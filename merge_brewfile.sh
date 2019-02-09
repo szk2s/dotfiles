a8b57c24f3077c85fbfc716b14fb5595c3b60bed
szk2s <suzukisjss@gmail.com>
2019-02-09 19:30:07 +0900

    Editing script for controlling the order
    - incomplete

--- merge_brewfile.sh
+++ merge_brewfile.sh
@@ -1,6 +1,32 @@
+# Incomplete (shold append and delete line by line)
 
 if 
-    cat ~/Brewfile >> ${DOT_DIRECTORY}/Brewfile && sort -r -u -o ${DOT_DIRECTORY}/Brewfile ${DOT_DIRECTORY}/Brewfile; 
+    cat ~/Brewfile >> ${DOT_DIRECTORY}/Brewfile && sort -u -r -o ${DOT_DIRECTORY}/Brewfile ${DOT_DIRECTORY}/Brewfile; 
+    IFS=$'\n'
+    for j in $(cat ${DOT_DIRECTORY}/Brewfile)    
+    do
+        if [[ "$j" =~ tap.* ]]; then
+            echo "0"
+        fi
+    done
+    for j in $(cat ${DOT_DIRECTORY}/Brewfile)    
+    do
+        if [[ "$j" =~ brew.* ]]; then
+            echo "1"
+        fi
+    done
+    for j in $(cat ${DOT_DIRECTORY}/Brewfile)    
+    do
+        if [[ "$j" =~ cask.* ]]; then
+            echo "2"
+        fi
+    done
+    for j in $(cat ${DOT_DIRECTORY}/Brewfile)    
+    do
+        if [[ "$j" =~ mas.* ]]; then
+            echo "3"
+        fi
+    done
 then
     echo $(tput setaf 2)Merge success! ✔︎$(tput sgr0)
 fi
